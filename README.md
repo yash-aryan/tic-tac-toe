ed24959b1af5b9c7a53f9d8520699ba7a359b439
Yash <79582883+yash-aryan@users.noreply.github.com>
2023-06-19 21:21:57 +0530

    Add Game Status, Result Modal & Update Logic
    - Updated the scripts.js logic & and cleaned up the code for readability.
    - Added result modal and in-game status to show player turns and game's
    outcome - who's the winner or tie.
    - Added styling to the new DOM elements.
    - Overall cleaned up unnecessary code.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# tic-tac-toe
-This is a project I worked on while doing "The Odin Project". Features a working prototype of tic-tac-toe game.
+# tic-tac-toe - Overview
+This is a project I worked on while doing The Odin Project's Javascript Path Course.
+It features a working prototype of tic-tac-toe game, that currently can only be played against 2 players offline.
 
 - <a href="https://yash-aryan.github.io/tic-tac-toe/" target="_blank">View Live Demo Here</a>
+
+# Techniques learnt and utilized
+- Closures and keeping object properties & variables private.
+- Factory functions & module pattern.
+- <a href="https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe#assignment" target="_blank">View the assignment here</a>
--- scripts.js
+++ scripts.js
@@ -1,63 +1,29 @@
 // Custom Module Function
+"use strict";
 const gameBoard = (() => {
 	const _gridCells = Array.from(document.querySelectorAll(".grid-cell"));
+	const _restartModal = document.querySelector(".restart-modal");
+	const _bgOverlay = document.querySelector(".bg-overlay");
+
 	const _board = ["", "", "", "", "", "", "", "", ""];
 	const _player1 = { name: "player1", marker: "X" };
 	const _player2 = { name: "player2", marker: "O" };
 	let _currentPlayer = _player1;
-	let _guideIndex = null;
+	let _outcome;
+	_dispTurnStatus();
 
-	function _changeCurrentPlayer() {
-		if (_currentPlayer === _player1) _currentPlayer = _player2;
-		else _currentPlayer = _player1;
-	}
+	(function () {
+		_gridCells.forEach(element => {
+			element.addEventListener("click", _setMarkerOnCell);
+		});
 
-	function getCurrentPlayer() {
-		return _currentPlayer;
-	}
-	// Check if someone won & return player's object, otherwise return null
-	function _winnerFoundYet() {
-		// Top & Left
-		if (
-			((_board[0] === _board[1] && _board[0] === _board[2]) ||
-				(_board[0] === _board[3] && _board[0] === _board[6])) &&
-			_board[0] !== ""
-		) {
-			_guideIndex = 0;
-			return true;
+		function _setMarkerOnCell() {
+			let clickedIndex = _gridCells.indexOf(this);
+			_setMarker(clickedIndex);
 		}
-		// Bottom & Right
-		else if (
-			((_board[8] === _board[7] && _board[8] === _board[6]) ||
-				(_board[8] === _board[5] && _board[8] === _board[2])) &&
-			_board[8] !== ""
-		) {
-			_guideIndex = 8;
-			return true;
-		}
-		// Middle & Diagonals
-		else if (
-			((_board[2] === _board[4] && _board[2] === _board[6]) ||
-				(_board[0] === _board[4] && _board[0] === _board[8]) ||
-				(_board[3] === _board[4] && _board[3] === _board[5]) ||
-				(_board[1] === _board[4] && _board[1] === _board[7])) &&
-			_board[4] !== ""
-		) {
-			_guideIndex = 4;
-			return true;
-		} else return null;
-	}
+	})();
 
-	function _getWinner(index) {
-		switch (_board[index]) {
-			case "X":
-				return _player1;
-			case "O":
-				return _player2;
-		}
-	}
-
-	function setMarker(index) {
+	function _setMarker(index) {
 		// Is the grid cell already filled? If yes then exit the function
 		if (_isGridCellTaken(index)) return;
 		// Add marker class in DOM, which holds svg for Xs & Os
@@ -71,12 +37,12 @@ const gameBoard = (() => {
 		}
 		// Add marker to _board Array as well
 		_board[index] = _currentPlayer.marker;
-		_changeCurrentPlayer();
-		// if statement only runs if winner is found
-		if (!!_winnerFoundYet()) {
-			console.log(`WINNER IS ${_getWinner(_guideIndex).marker}!!`);
-			_resetGame();
-		}
+		// Change current player
+		if (_currentPlayer === _player1) _currentPlayer = _player2;
+		else _currentPlayer = _player1;
+		// After marker is set, following functions are called
+		_checkForOutcome();
+		_dispTurnStatus();
 	}
 
 	function _isGridCellTaken(index) {
@@ -84,31 +50,108 @@ const gameBoard = (() => {
 		else return false;
 	}
 
+	function _checkForOutcome() {
+		if (_board.every(element => element !== "")) {
+			_outcome = "TIE";
+			_openModal();
+		} else {
+			_checkForWinner();
+		}
+	}
+
+	// Check if someone won by checking patterns
+	function _checkForWinner() {
+		const _getWinner = index => {
+			switch (_board[index]) {
+				case "X":
+					_outcome = _player1.marker;
+					break;
+				case "O":
+					_outcome = _player2.marker;
+					break;
+			}
+			_openModal();
+		};
+		// Check for matching patterns for:-
+		// Top & Left
+		if (
+			((_board[0] === _board[1] && _board[0] === _board[2]) ||
+				(_board[0] === _board[3] && _board[0] === _board[6])) &&
+			_board[0] !== ""
+		)
+			_getWinner(0);
+		// Bottom & Right
+		else if (
+			((_board[8] === _board[7] && _board[8] === _board[6]) ||
+				(_board[8] === _board[5] && _board[8] === _board[2])) &&
+			_board[8] !== ""
+		)
+			_getWinner(8);
+		// Middle & Diagonals
+		else if (
+			((_board[2] === _board[4] && _board[2] === _board[6]) ||
+				(_board[0] === _board[4] && _board[0] === _board[8]) ||
+				(_board[3] === _board[4] && _board[3] === _board[5]) ||
+				(_board[1] === _board[4] && _board[1] === _board[7])) &&
+			_board[4] !== ""
+		)
+			_getWinner(4);
+	}
+
+	function _openModal() {
+		_dispResultStatus();
+		_restartModal.style.display = "flex";
+		_bgOverlay.style.display = "block";
+		document
+			.querySelector(".btn-restart")
+			.addEventListener("click", _resetGame);
+	}
+
+	function _closeModal() {
+		_restartModal.style.display = "none";
+		_bgOverlay.style.display = "none";
+	}
+
 	function _resetGame() {
+		// reset _board Array
 		for (let i = 0; i < _board.length; i++) {
 			_board[i] = "";
 		}
-		_guideIndex = null;
 		_currentPlayer = _player1;
 		_gridCells.forEach(element => {
 			element.classList.remove("markedX", "markedO");
 		});
+		_dispTurnStatus();
+		_closeModal();
 	}
 
-	return {
-		setMarker,
-		getCurrentPlayer
-	};
-})();
+	function _dispTurnStatus() {
+		const headerX = document.querySelector("#x-turn");
+		const headerO = document.querySelector("#o-turn");
+		switch (_currentPlayer.marker) {
+			case "X":
+				headerO.classList.remove("current-turn");
+				headerX.classList.add("current-turn");
+				break;
+			case "O":
+				headerX.classList.remove("current-turn");
+				headerO.classList.add("current-turn");
+				break;
+		}
+	}
 
-const initGame = (() => {
-	const _gridCells = Array.from(document.querySelectorAll(".grid-cell"));
-	_gridCells.forEach(n => {
-		n.addEventListener("click", _setMarkerOnCell);
-	});
-
-	function _setMarkerOnCell() {
-		let clickedIndex = _gridCells.indexOf(this);
-		gameBoard.setMarker(clickedIndex);
+	function _dispResultStatus() {
+		const headerResult = document.querySelector("#result-status");
+		switch (_outcome) {
+			case "X":
+				headerResult.textContent = `The winner is ${_player1.name}!`;
+				break;
+			case "O":
+				headerResult.textContent = `The winner is ${_player2.name}!`;
+				break;
+			case "TIE":
+				headerResult.textContent = `It's a tie!`;
+				break;
+		}
 	}
 })();
